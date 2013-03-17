c9533e343e93b83c4d2216fbf56cb6fb422706ed
Joe Wright <joe@joejag.com>
2013-03-17 21:26:14 +0000

    Forgot to add JQuery

--- wordpress-theme/footer.php
+++ wordpress-theme/footer.php
@@ -5,6 +5,7 @@
                 <!-- Le javascript
                 ================================================== -->
                 <!-- Placed at the end of the document so the pages load faster -->
+                <script type='text/javascript' src='http://code.joejag.com/wp-includes/js/jquery/jquery.js?ver=1.8.3'></script>
                 <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
                 <script type="text/javascript" src="/j/highlight/sh_main.min.js"></script>
                 <script type="text/javascript" src="/j/highlight/sh_java.js"></script>
